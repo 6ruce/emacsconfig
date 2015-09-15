2b3e52e7c9806aa3b43ad6af3193384f359cd4c6
Vitaly Braslavsky <i6ruce@hotmail.com>
2015-09-16 01:47:25 +0300

    Updating Emacs enviroment

--- emacssync.bat
+++ emacssync.bat
@@ -1,26 +1,22 @@
-REM @echo off
+@echo off
 set command=%1
 set curPath=%~dp0
 IF "%command%"=="sync" (
 	call git pull origin master
-	copy "%curPath%.emacs" "%curPath%../../.emacs" /Y 
-	rmdir "%curPath%../elpa" /S /Q
-	xcopy "%curPath%/elpa" "%curPath%../elpa" /S /I
+	copy "%curPath%.emacs" "%curPath%..\..\.emacs" /Y 
+	rmdir "%curPath%..\elpa" /S /Q
+	xcopy "%curPath%/elpa" "%curPath%..\elpa" /S /I
 ) ELSE IF "%command%"=="syncb" (
-	copy "%curPath%../../.emacs" "%curPath%.emacs" /Y 
+	copy "%curPath%..\..\.emacs" "%curPath%.emacs" /Y 
 	rmdir elpa /S /Q
-	xcopy "%curPath%../elpa" "%curPath%/elpa" /S /I
+	xcopy "%curPath%..\elpa" "%curPath%/elpa" /S /I
 	call git add .emacs 
 	call git add emacssync.bat 
 	call git add elpa/*
 	call git status
-	call git commit -m "Updating Emac enviroment"
+	call git commit -m "Updating Emacs enviroment"
 	call git push origin master
-) ELSE IF "%commad%"=="updated" (
-	git pull origin master
-	copy _vimrc ../_vimrc /Y 
 ) ELSE (
   echo Commands:
-  echo     sync - synchronizes files in `sync` dir and vim files
-
+  echo     sync - synchronizes files in `sync` dir and emacs files
 )
